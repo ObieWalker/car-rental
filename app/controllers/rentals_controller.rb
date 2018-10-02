138137988b93ecbb3f96ad4c35e46721b49fdf3f
ObieWalker <obinnawalker@gmail.com>
2018-10-02 12:35:07 +0100

    Update app with basic features - add seed file - configure login and registration feature - conditionally render a user rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,7 +4,7 @@ class RentalsController < ApplicationController
   # GET /rentals
   # GET /rentals.json
   def index
-    @rentals = Rental.all
+    @rentals = Rental.where(:user_id => current_user.id)
   end
 
   # GET /rentals/1
